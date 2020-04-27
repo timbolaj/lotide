73f1515be42e71b96ec8af7f89bd13e1860bdc15
Aaron Timbol <timbolaj@gmail.com>
2020-04-27 23:10:50 +0000

    head function

--- head.js
+++ head.js
@@ -10,7 +10,8 @@ const head = function(arr) {
   return arr[0];
 }
 
+//TEST CASES
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
+
