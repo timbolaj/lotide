b7a5f834112a8053c6ea928131d26b5cd3c82474
Aaron Timbol <timbolaj@gmail.com>
2020-04-28 20:57:31 +0000

    edited without function

--- without.js
+++ without.js
@@ -12,7 +12,7 @@ const eqArrays = function(arr1, arr2) {
   }
   //if none of the false cases were satisfied, then equality is maintained between arr1 and arr2
   return true;
-}
+};
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -20,37 +20,33 @@ const assertArraysEqual = function(arr1, arr2) {
   } else {
     console.log('The arrays are not equal');
   }
-}
+};
 
 /*
-Implement without which will return a subset of a given array, removing unwanted elements.
-This function should take in a source array and a itemsToRemove array. It should return a 
-new array with only those elements from source that are not present in the itemsToRemove array.
-Source array should not be modified
+Implement without which will return a subset of a given array, removing unwanted elements. This function should take in a source array and a itemsToRemove array. It should return a new array with only those elements from source that are not present in the itemsToRemove array. Source array should not be modified
 */
 
 //Find the unwanted elements that are present in the source array
 const without = function(sourceArr, itemsToRemove) {
   let wantedItems = [];
-  for (item of sourceArr) {
+  for (let item of sourceArr) {
     if (!itemsToRemove.includes(item)) {
-      wantedItems.push(item)
+      wantedItems.push(item);
     }
   }
   return wantedItems;
-}
-//
+};
+
 let source = [1,2,3];
 let dontWant = [1, '2'];
-let expected = [2, 3]
+let expected = [2, 3];
 console.log(assertArraysEqual(expected,without(source,dontWant)));
 console.log(assertArraysEqual(source, [1,2,3]));
 source = ['1', '2'];
 dontWant = [1,2];
 expected = ['1', '2'];
 console.log(assertArraysEqual(expected,without(source,dontWant)));
-console.log
 source = ['hello', 2];
 dontWant = ['hello'];
-expected = [2]
+expected = [2];
 console.log(assertArraysEqual(expected,without(source,dontWant)));
