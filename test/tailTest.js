0f8cacd2cf57cfc70cae65cd17190f06deaad667
Aaron Timbol <timbolaj@gmail.com>
2020-05-04 18:05:19 +0000

    tailTest refactored to use chai and mocha

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,21 @@
-//Export tail.js and assertEqual.js
+//Export tail.js and chai
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+describe('test should return all elements in an array but the first element', () => {
+  it ("return array ['lighthouse', 'labs'] ", () => {
+    const arrOfStrs = tail(['hello', 'lighthouse', 'labs']);
+    const actualTail = ['lighthouse', 'labs'];
+    assert.deepEqual(arrOfStrs, actualTail);
+  });
+
+  it ("return empty array for one argument in function", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+
+  it("return empty array for no argument in function", () => {
+    assert.deepEqual(tail([]), []);
+  })
+})
 
 //TEST CASES
-const result = tail(['hello', 'lighthouse', 'labs']);
-assertEqual(result.length, 2);
-assertEqual(result[0], 'lighthouse');
-assertEqual(result[1], 'labs');
-
-const oneThing = tail([0]);
-assertEqual(oneThing.length, 0);
-assertEqual(oneThing[0], undefined);
-
-const nothing = tail([]);
-assertEqual(nothing[0], undefined);
