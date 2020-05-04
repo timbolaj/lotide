ca01d9b2e6f1d3fe25d7047fcb70a5d2ee43199e
Aaron Timbol <timbolaj@gmail.com>
2020-05-04 18:15:01 +0000

    refactored middleTest with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,24 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-let oneElement = middle([1]);
-let twoElements = middle([1,2]);
-let threeElements = middle([1,2,3]);
-let oddArray = middle([1,2,3,4,5,]);
-let evenArray = middle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+describe('function should return middle-most elements of an array. If length > 2 and odd length, return 1 value, otherwise return two.', () => {
+  it ('return empty array for one element in function', () => {
+    let oneElement = middle([1])
+    assert.deepEqual(oneElement, []);
+  });
 
-assertArraysEqual([], oneElement);
-assertArraysEqual([], twoElements);
-assertArraysEqual([2], threeElements);
-assertArraysEqual([3], oddArray);
-assertArraysEqual([8,9], evenArray);
+  it ('return empty array for two elements in function', () => {
+    let twoElements = middle([1,2])
+    assert.deepEqual(twoElements, []);
+  });
+
+  it ('return [2] for array [1,2,3]', () => {
+    let shouldBeTwo = middle([1,2,3])
+    assert.deepEqual(shouldBeTwo, [2]);
+  });
+
+  it ('return [8,9] for array [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]', () => {
+    let returnTwo = middle([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+    assert.deepEqual(returnTwo, [8,9])
+  })
+})
