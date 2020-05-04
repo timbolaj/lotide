b39dae5d6cd00dc6fda6d2b1112b4bd01eede0dd
Aaron Timbol <timbolaj@gmail.com>
2020-05-04 17:49:23 +0000

    Using chai and mocha for testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,21 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
+describe('head should return the first element in the array', () => {
+  it ('returns 5 from arr [5,6,7]', () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it ("returns 'hello' from following ['hello', 'lighthouse', 'labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it ('returns 1 from an array of [1]', () => {
+    assert.strictEqual(head([1]), 1)
+  })
+})
+
 //TEST CASES
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head([1]), 1);
