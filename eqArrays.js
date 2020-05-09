fa5f047ffa191d490cd973e617337c0e716fdcc0
Aaron Timbol <timbolaj@gmail.com>
2020-05-09 23:31:56 +0000

    added functionality to handle cases including nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,40 @@
-//Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
-const eqArrays = function(arr1, arr2) {
-  //make sure length is the same
-  if (arr1.length === arr2.length) {
-    //for loop to make sure each element matches in the right spot
-    for (let i = 0; i < arr1.length; i ++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  } else {
+//helper function to check for nested arrays
+const checkForNestArrays = (arr1) => {
+  if (!Array.isArray(arr1)) {
     return false;
   }
-  //if none of the false cases were satisfied, then equality is maintained between arr1 and arr2
-  return true;
+
+  for (const element of arr1) {
+    if (Array.isArray(element)) {
+      return true;
+    }
+  }
+  return false;
 };
 
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  if (checkForNestArrays(arr1) !== checkForNestArrays(arr2)) {
+    return false;
+  }
+
+  for (const index in arr1) {
+    let element1 = arr1[index];
+    let element2 = arr2[index];
+    if (!Array.isArray(element1)) {
+      if (element1 !== element2) {
+        return false;
+      } else {
+        arr1.slice(index, 1);
+        arr2.slice(index, 1);
+      }
+    } else {
+      return eqArrays(element1, element2);
+    }
+  }
+  return true;
+};
 module.exports = eqArrays;
